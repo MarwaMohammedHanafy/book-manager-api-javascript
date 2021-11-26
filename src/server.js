1da12796e10ac071c2868eddc404cc76f87464d5
Marwa <marwama3bade@gmail.com>
2021-11-26 18:10:13 +0000

    change process.env..port to process.env..PORT in server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ console.log(`environment = ${environment}`);
 require('dotenv').config({ path: `.env.${environment}` });
 const app = require('./app');
 const populateDummyData = require('./database/database_seed');
-app.listen(process.env.port, () => {
-  console.log(`Express started on port ${process.env.port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Express started on port ${process.env.PORT}`);
   // Seed the database with some data
   if (environment === 'dev') {
     populateDummyData();
