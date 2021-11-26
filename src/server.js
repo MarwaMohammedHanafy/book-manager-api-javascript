0a9d3094804bd36c48bb8ff519f0bbef8900ac37
Marwa <marwama3bade@gmail.com>
2021-11-26 11:26:33 +0000

    Utilising Node env to choose the configuration development and production

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const environment = process.env.NODE_ENV || 'dev';
 console.log(`environment = ${environment}`);
-require('dotenv').config({ path: `.env` });
+require('dotenv').config({ path: `.env.${environment}` });
 const app = require('./app');
 const populateDummyData = require('./database/database_seed');
 app.listen(process.env.port, () => {
-  console.log(`Express started on port ${JSON.stringify(process.env.port)}`);
+  console.log(`Express started on port ${process.env.port}`);
   // Seed the database with some data
   if (environment === 'dev') {
     populateDummyData();
