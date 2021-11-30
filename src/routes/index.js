026079a5beee8a1cb515475a5606e3f76532e644
Marwa <marwama3bade@gmail.com>
2021-11-30 17:14:08 +0000

    Add Swager Documentation in swagger.yaml

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {booksController} = require('../controllers');
+const { booksController } = require('../controllers');
 
 const router = new express.Router();
 router.get('/books', booksController.getBooks);
+
 router.get('/books/:bookId', booksController.getBook);
 router.post('/books', booksController.saveBook);
 
